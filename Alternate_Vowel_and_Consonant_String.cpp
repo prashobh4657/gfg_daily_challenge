9d8c5b7e469f2a661bce96cd53424c67bbafa2ec
Prashobh <81906847+prashobh4657@users.noreply.github.com>
2022-12-04 12:21:21 +0530

    Create Alternate_Vowel_and_Consonant_String.cpp

---
+++ Alternate_Vowel_and_Consonant_String.cpp
@@ -0,0 +1,14 @@
+
+// Question Link : https://practice.geeksforgeeks.org/problems/alternate-vowel-and-consonant-string2939/1
+// Submission 1 : 2022-12-04 11:54:34	Time Limit Exceeded	cpp	1016 / 1023
+    /*
+    ==> Vowel, consonant are strings all characters which are vowel,consonant in the string s respectively.
+    ==> Space complexity : O(v+c) = O(n) where v=vowel.size() and c=consonant.size();
+    ==> Time complexity : O(nlogn) (Sorting of both vowel,consonant strings);
+    */
+// Submission 2 :
+   //this implementation using frequency array for both vowel and consonant later will implement;
+   /*
+    Space complexity : O(2*26);
+    
+   */
