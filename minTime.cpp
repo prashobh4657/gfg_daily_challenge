5d2bcc64308a5630de45647ab62253cb4fbf8ced
Prashobh <81906847+prashobh4657@users.noreply.github.com>
2023-06-16 15:46:25 +0530

    Create minTime.cpp

---
+++ minTime.cpp
@@ -0,0 +1,3 @@
+// Link : https://practice.geeksforgeeks.org/problems/95bb244da24edd6214086ff934886ccda6ed9da8/1
+// See Submission : 2023-06-16 15:17:01	Wrong	cpp	1 / 1120	  ==> Failed on given testcase 2 (Tell why greedy will not work);
+// See Submission : 2023-06-16 15:44:31	Correct	cpp	1120 / 1120 ==> DP; (Two call for each type either to go left side or right side); 
